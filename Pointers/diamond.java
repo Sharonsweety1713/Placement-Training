6d425899cf18e517ca67680b76d879d0e9baae75
Sharonsweety1713 <117472753+Sharonsweety1713@users.noreply.github.com>
2024-10-16 12:12:39 +0530

    Create diamond.java

---
+++ Pointers/diamond.java
@@ -0,0 +1,45 @@
+import java.util.*;
+class Main{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+       
+        for(int i=0;i<=a;i++)
+        {
+            for(int j=i;j<=b;j++)
+            {
+                
+                System.out.print(" ");
+               
+            }
+            for(int j=0;j<=i;j++)
+            {
+                
+                System.out.print("* ");
+               
+            }
+           
+            System.out.println();
+        }
+        for(int i=1;i<=a;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                
+                System.out.print(" ");
+               
+            }
+            for(int j=i;j<=b;j++)
+            {
+                
+                System.out.print("* ");
+               
+            }
+           
+            System.out.println();
+        }
+        
+    }
+}
